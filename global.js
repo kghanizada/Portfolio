19528b5af19e44382199ea2cb7ddc7f5f44147df
kghanizada <kg7952@mit.edu>
2024-02-26 08:55:58 -0500

    changes

--- global.js
+++ global.js
@@ -3,3 +3,48 @@ console.log("IT’S ALIVE!");
 function $$ (selector, context = document) {
 	return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+	{url: "index.html", title: "Home"},
+	{url: "projects/index.html", title: "Projects"},
+	{url: "resume/index.html", title: "Resume"},
+	{url: "contact/index.html", title: "Contact"},
+	/* add the rest of your pages here */
+];
+
+
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+
+let nav = document.createElement("nav");
+
+document.body.prepend(nav);
+
+for (let p of pages) {
+	let url = p.url;
+	let title = p.title;
+	// TODO create link and add it to nav
+	
+    url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
+
+	let a = document.createElement("a");
+	a.href = url;
+	a.textContent = title;
+
+	a.classList.toggle("current", a.host === location.host && a.pathname === location.pathname);
+
+	if (a.host === location.host && a.pathname === location.pathname) {
+		a.classList.add("current");
+	}
+	
+
+
+	nav.append(a);
+}
+
+
+
+
+
+
+
+
